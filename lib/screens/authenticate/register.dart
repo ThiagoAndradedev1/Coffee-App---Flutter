2c7dcc031f42df546d463430df59805b6e35c03f
thiago.andrade.dev1@gmail.com <thiago.andrade.dev1@gmail.com>
2021-02-16 18:30:09 -0300

    first commit

---
+++ lib/screens/authenticate/register.dart
@@ -0,0 +1,137 @@
+import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
+import 'package:flutter/material.dart';
+import 'package:brew_crew/services/auth.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Register extends StatefulWidget {
+  final Function toggleView;
+  Register({this.toggleView});
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
+
+  String email = "";
+  String password = "";
+  String error = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[200],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[800],
+              elevation: 0.0,
+              title: Text(
+                'Sign up to Coffe Break',
+                style: GoogleFonts.comfortaa(
+                  textStyle: TextStyle(
+                    letterSpacing: .5,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              actions: <Widget>[
+                FlatButton.icon(
+                  icon: Icon(Icons.person, color: Colors.yellow[800]),
+                  label: Text('Sign In',
+                      style: TextStyle(color: Colors.yellow[800])),
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                )
+              ],
+            ),
+            body: SingleChildScrollView(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: 300,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                'https://cdn.pixabay.com/photo/2020/07/08/19/01/coffee-5384864_960_720.png'),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        decoration: textInputDecoration.copyWith(
+                          hintText: 'Email',
+                          prefixIcon: Icon(
+                            Icons.email,
+                          ),
+                        ),
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter an email' : null,
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        decoration: textInputDecoration.copyWith(
+                            hintText: 'Password',
+                            prefixIcon: Icon(
+                              Icons.lock,
+                            )),
+                        obscureText: true,
+                        validator: (val) => val.length < 6
+                            ? 'Enter a password 6+ chars long'
+                            : null,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      Container(
+                        width: double.infinity,
+                        child: RaisedButton(
+                          color: Colors.grey[800],
+                          padding: EdgeInsets.symmetric(horizontal: 50.0),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Colors.grey[800])),
+                          child: Text(
+                            "Register",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              setState(() => loading = true);
+                              dynamic result =
+                                  await _auth.registerWithEmailAndPassword(
+                                      email, password);
+                              if (result == null) {
+                                setState(() {
+                                  error = "please supply a valid email";
+                                  loading = false;
+                                });
+                              }
+                            }
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 12.0),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 14.0),
+                      )
+                    ],
+                  ),
+                )),
+          );
+  }
+}
---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,156 @@
+import 'dart:ffi';
+
+import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SignIn extends StatefulWidget {
+  final Function toggleView;
+  SignIn({this.toggleView});
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
+  bool loading = false;
+
+  String email = "";
+  String password = "";
+  String error = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[200],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[800],
+              elevation: 0.0,
+              title: Text(
+                'Sign in to Coffee Break',
+                style: GoogleFonts.comfortaa(
+                  textStyle: TextStyle(
+                    letterSpacing: .5,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              actions: <Widget>[
+                FlatButton.icon(
+                  color: Colors.brown[800],
+                  icon: Icon(
+                    Icons.person,
+                    color: Colors.yellow[800],
+                  ),
+                  label: Text('Register',
+                      style: TextStyle(color: Colors.yellow[800])),
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                )
+              ],
+            ),
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 300,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            'https://cdn.pixabay.com/photo/2020/07/08/19/01/coffee-5384864_960_720.png'),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 50.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 20.0),
+                          TextFormField(
+                            decoration: textInputDecoration.copyWith(
+                              hintText: 'Email',
+                              prefixIcon: Icon(Icons.email),
+                            ),
+                            validator: (val) =>
+                                val.isEmpty ? 'Enter an email' : null,
+                            onChanged: (val) {
+                              setState(() => email = val);
+                            },
+                          ),
+                          SizedBox(height: 20.0),
+                          TextFormField(
+                            decoration: textInputDecoration.copyWith(
+                              hintText: 'Password',
+                              prefixIcon: Icon(
+                                Icons.lock,
+                              ),
+                            ),
+                            obscureText: true,
+                            validator: (val) => val.length < 6
+                                ? 'Enter a password 6+ chars long'
+                                : null,
+                            onChanged: (val) {
+                              setState(() => password = val);
+                            },
+                          ),
+                          SizedBox(height: 20.0),
+                          Container(
+                            width: double.infinity,
+                            child: RaisedButton(
+                              color: Colors.grey[800],
+                              padding: EdgeInsets.symmetric(horizontal: 50.0),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18.0),
+                                  side: BorderSide(color: Colors.grey[800])),
+                              child: Text(
+                                "Sign In",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              onPressed: () async {
+                                if (_formKey.currentState.validate()) {
+                                  setState(() => loading = true);
+                                  dynamic result =
+                                      await _auth.signInWithEmailAndPassword(
+                                          email, password);
+                                  if (result == null) {
+                                    setState(
+                                      () {
+                                        error =
+                                            "COULD NOT SIGN IN WITH THOSE CREDENTIALS";
+                                        loading = false;
+                                      },
+                                    );
+                                  }
+                                }
+                              },
+                            ),
+                          ),
+                          SizedBox(height: 12.0),
+                          Text(
+                            error,
+                            style: TextStyle(color: Colors.red, fontSize: 14.0),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+  }
+}
